69fa67f7855338e9dd705243f4949f40444e6629
Tahsina Laboni <tahsinaa404@gmail.com>
2021-08-27 18:21:08 +0600

    first commit

---
+++ api.js
@@ -0,0 +1,57 @@
+
+function loadData(){
+                        
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json.title))
+};
+
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => displayUsers(data));
+};
+
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then( data => console.log(data));
+};
+
+
+function displayUsers(data){
+    const ul = document.getElementById('users-ul');
+
+
+  for(const user of data){
+      console.log(user.name);
+      const li = document.createElement('li');
+      li.innerText = `name: ${user.name},email: ${user.email}`;
+      ul.appendChild(li);
+
+  }
+}
+//-----------------practice
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => displayLoad(data));
+};
+
+function displayLoad(data){
+    const ul = document.getElementById("users-ul");
+    for( const user of data){
+        const li = document.createElement('li');
+        li.innerText = `name: ${user.name} ${user.email}`;
+        ul.appendChild(li);
+    }
+
+}
